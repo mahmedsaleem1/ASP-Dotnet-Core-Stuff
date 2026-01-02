7fce3bd36bb956061b0fc610a6b067f2e8302942
mahmedsaleem1 <mahmedsaleem786@gmail.com>
2026-01-02 19:08:14 +0500

    Refactored the program.cs file, Endpoints

---
+++ Endpoints/GamesEndpoints.cs
@@ -0,0 +1,72 @@
+using System;
+
+namespace GameStore.Api.Endpoints;
+
+// Extension method -> in static class
+// static class can have static methods
+public static class GamesEndpoints
+{
+    const string FetchGameEndpoint = "GetGame";
+
+    private static List<GameDto> games = [
+    new (1, "Grand Theft Auto V", "Action-adventure", 29.99m, new DateOnly(2013, 9, 17)),
+    new (2, "Grand Theft Auto IV", "Action-adventure", 19.99m, new DateOnly(2009, 9, 17)),
+    new (1, "Grand Theft Auto III", "Action-adventure", 9.99m, new DateOnly(2003, 9, 17)),
+    ];
+
+    public static void MapGamesEndpoints(this WebApplication app) {
+
+        var group = app.MapGroup("/games");
+
+        group.MapGet("/all", () => games);
+
+        group.MapGet("/{id}", (int id) =>
+        {
+            var game = games.Find(g => g.Id == id); 
+
+            return game is null ? Results.NotFound() : Results.Ok(game);
+        }).WithName(FetchGameEndpoint);
+
+        group.MapPost("/add", (CreateGameDto newGame) =>
+        {
+            GameDto game = new (
+                games.Count + 1,
+                newGame.Name,
+                newGame.Genre, 
+                newGame.Price,
+                newGame.ReleaseDate
+            );
+
+            games.Add(game);
+
+            return Results.CreatedAtRoute(FetchGameEndpoint, new {id = game.Id}, game);
+        });
+
+        group.MapPut("/update/{id}" , (int id, UpdateGameDto updateGame) =>
+        {
+            var index = games.FindIndex(game => game.Id == id);
+
+            if (index == -1)
+            {
+                return Results.NotFound();
+            }
+
+            games[index] = new GameDto(
+                id,
+                updateGame.Name,
+                updateGame.Genre,
+                updateGame.Price,
+                updateGame.ReleaseDate
+            );
+
+            return Results.NoContent();
+        });
+
+        group.MapDelete("/{id}", (int id) =>
+        {
+            games.RemoveAll(game => game.Id == id);
+
+            return Results.NoContent();
+        });
+    }
+}
